4ed75a208612a3cc2c76b284e4123d1dfa1cd124
rizanw <rzkandre@gmail.com>
2020-05-07 23:05:31 +0700

    init

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+from maktabah import getResult
+import time
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index(): 
+    return render_template('index.html')
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    if request.args.get('search'):
+        start_time = time.time()
+        search = request.args['search']
+        print(search)
+        print("================START=======================")
+        res = getResult(search)
+        print("=====================END====================")
+        print(res)
+        len_result = len(res)
+        total_time = (time.time() - start_time)
+        return render_template('result.html', results = res, query = search, totalTime = total_time, lenResult = len_result)
+    else:
+        return redirect('/')
+
+app.run(debug = True) 
