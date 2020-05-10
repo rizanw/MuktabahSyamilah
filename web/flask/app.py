ed6fbd6ce6578e1a5cee52c672eab3765e66d329
rizanw <rzkandre@gmail.com>
2020-05-10 08:03:54 +0700

    flask done

--- web/flask/app.py
+++ web/flask/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from maktabah import getResult
 import time
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config.from_pyfile('config.py')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -15,15 +16,31 @@ def result():
         start_time = time.time()
         search = request.args['search']
         print(search)
-        print("================START=======================")
-        res = getResult(search)
-        print("=====================END====================")
-        print(res)
-        len_result = len(res)
-        total_time = (time.time() - start_time)
-        return render_template('result.html', results = res, query = search, totalTime = total_time, lenResult = len_result)
+        if ('\u0600' <= search <= '\u06FF' or
+            '\u0750' <= search <= '\u077F' or
+            '\u08A0' <= search <= '\u08FF' or
+            '\uFB50' <= search <= '\uFDFF' or
+            '\uFE70' <= search <= '\uFEFF' or
+            '\U00010E60' <= search <= '\U00010E7F' or
+            '\U0001EE00' <= search <= '\U0001EEFF'):
+            print("================START=======================")
+            res = getResult(search)
+            print("=====================END====================")
+            print(res)
+            len_result = len(res)
+            total_time = (time.time() - start_time)
+            return render_template('result.html', results = res, query = search, totalTime = total_time, lenResult = len_result)
+        flash('ARABIC ONLY!', 'error')
+        return redirect('/')
     else:
         return redirect('/')
+
+@app.route('/result/detail', methods=['GET', 'POST'])
+def detail():
+    namakitab = request.args.get('kitab')
+    halaman = request.args.get('hal')
+    isikitab = request.args.get('data')
+    return render_template('detail.html', namakitab=namakitab, halaman=halaman, isikitab=isikitab)
 
 if __name__ == "__main__":
     app.run(debug=True) 
--- web/flask/maktabah.py
+++ web/flask/maktabah.py
@@ -1,13 +1,23 @@
 import re
 import time
+import sys
+import os
 import numpy as np
 import unicodedata as ud
 from gensim.models import FastText as ft
 from pyarabic.araby import strip_tashkeel
-from sklearn.metrics.pairwise import cosine_similarity 
+from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 def normalizeArabic(text):
     text = re.sub("[إأٱآا]", "ا", text)
@@ -17,80 +27,94 @@ def normalizeArabic(text):
     text = re.sub("\[", "", text)
     text = re.sub("\]", "", text)
     return(text)
+    
 
 def getResult(text):
-  print("START READ AND PREPROCESSING")
+    print("START READ AND PREPROCESSING", text)
+    # kitab = np.load('dataset/numpy/kitabsave.npy', allow_pickle=True)
+    kitab = np.load(resource_path('./data/dataset/kitabsave.npy'), allow_pickle=True)
+    kitab = kitab.tolist()
+    # sentence_clear = np.load('dataset/numpy/sentence_clearsave.npy', allow_pickle=True)
+    sentence_clear = np.load(resource_path('./data/dataset/sentence_clearsave.npy'), allow_pickle=True)
+    sentence_clear = sentence_clear.tolist()
+    # kategori = np.load('dataset/numpy/kategori.npy', allow_pickle=True)
+    kategori = np.load(resource_path('./data/dataset/kategori.npy'), allow_pickle=True)
+    kategori = kategori.tolist()
+    # namakitab = np.load('dataset/numpy/namakitab.npy', allow_pickle=True)
+    namakitab = np.load(resource_path('./data/dataset/namakitab.npy'), allow_pickle=True)
+    namakitab = namakitab.tolist()
 
-  kitab = np.load('./data/dataset/kitabsave.npy')
-  kitab = kitab.tolist()
-  sentence_clear = np.load('./data/dataset/sentence_clearsave.npy')
-  sentence_clear = sentence_clear.tolist()
-  kategori = np.load('./data/dataset/kategori.npy')
-  kategori = kategori.tolist()
-  namakitab = np.load('./data/dataset/namakitab.npy')
-  namakitab = namakitab.tolist()
-  modelFT = ft.load('./data/model/modelFT.model')
+    # kitab[0][1]
+    # modelFT = ft.load('Model/modelFT.model')
+    modelFT = ft.load(resource_path('./data/model/modelFT.model'))
 
-  # MOST SIMILAR WE
-  hasilQE = modelFT.wv.most_similar(text)
-  hasilQE = [(strip_tashkeel(''.join(c for c in hasilQE[i][0] if not ud.category(c).startswith('P'))), hasilQE[i][1]) for i in range(len(hasilQE))]
-  print(hasilQE)
+    # MOST SIMILAR WE
+    hasilQE = modelFT.wv.most_similar(text)
+    hasilQE = [(strip_tashkeel(''.join(c for c in hasilQE[i][0] if not ud.category(c).startswith('P'))), hasilQE[i][1]) for i in range(len(hasilQE))]
+    print(hasilQE)
 
-  #TF-IDF
-  tfidf_vectorizer = TfidfVectorizer() 
-  
-  isikitab=''
-  ktb=[]
-  for i in range(len(kitab)):
-      for j in kitab[i] :
-          isikitab = ''.join(kitab[i])
-      ktb.append(isikitab)
-      
-  norm_tf=[]
-  for i in range(len(ktb)):
-      norm_tfidf = normalizeArabic(ktb[i])
-      norm_tf.append(norm_tfidf)
+    #TF-IDF
+    tfidf_vectorizer = TfidfVectorizer()
 
-  tfidf_doc = tfidf_vectorizer.fit_transform(norm_tf)
-  
-  # PIFQvectorizer = CountVectorizer()
-  # vectoreTF = PIFQvectorizer.fit_transform(norm_tf)
-  # featureTf = PIFQvectorizer.get_feature_names()
+    norm_tf=[]
+    for isikitab in kitab:
+        for ktb in isikitab:
+            norm_tfidf = normalizeArabic(ktb)
+            norm_tf.append(norm_tfidf)
 
-  cosim = [] 
-  nilaicosim = []
-  for i in hasilQE: 
-    tes=i[0]
-    print(tes)
-    tfidf_query = tfidf_vectorizer.transform([tes])
-    cos=0.0
-    #hitung kedekatan query pada masing masing dokumen 
-    cos=cosine_similarity(tfidf_doc,tfidf_query)
-    # print(type(cos))
-    cosim.append(max(cos))
-    nilaicosim.append(cos)
+    tfidf_doc = tfidf_vectorizer.fit_transform(norm_tf) 
+    PIFQvectorizer = CountVectorizer()
+    vectoreTF = PIFQvectorizer.fit_transform(norm_tf)
+    featureTf = PIFQvectorizer.get_feature_names()
 
-    # countTF = []
-    # s = ''.join(c for c in tes if not ud.category(c).startswith('P'))
-    # s = strip_tashkeel(s)
-    # for k in range(len(featureTf)):
-    #   if featureTf[k] == s:
-    #     # print(k)
-    #     for j in range(vectoreTF.shape[0]):
-    #       countTF.append(vectoreTF[j,k])
-    # if len(countTF) < 1 :
-    #   for j in range(vectoreTF.shape[0]):
-    #     countTF.append(0.0)
- 
-  finaloutput = []
-  print("======= hasil Cosim ===========")
-  angka = 0
-  for i in nilaicosim: 
-    for j in range(len(i)):
-      if i[j] == cosim[angka]:
-        print(kategori[0], '-', namakitab[j], '-', i[j])
-        finaloutput.append({'namakitab': namakitab[j], 'kategori': kategori[0]}) 
-    angka += 1
-  
-  return finaloutput 
-      
+    cosim = [] 
+    nilaicosim = []
+    hasilQEpakai = hasilQE[0:3]
+    for i in hasilQEpakai:
+        tes=i[0]
+        print(tes)
+
+        tfidf_query = tfidf_vectorizer.transform([tes])
+        cos=0.0
+        #hitung kedekatan query pada masing masing dokumen 
+        cos=cosine_similarity(tfidf_doc,tfidf_query)
+        # print(type(cos))
+        cosim.append(max(cos))
+        nilaicosim.append(cos)
+
+        # print('tfidf')
+        # ================
+        countTF = []
+        s = ''.join(c for c in tes if not ud.category(c).startswith('P'))
+        s = strip_tashkeel(s)
+        for k in range(len(featureTf)):
+            if featureTf[k] == s:
+            # print(k)
+                for j in range(vectoreTF.shape[0]):
+                    countTF.append(vectoreTF[j,k]) 
+
+    print("======= hasil Cosim ===========")
+    finaloutput = []
+    angka = 0
+    for i in nilaicosim:
+        print(hasilQE[angka][0])
+        for j in range(len(i)):
+            if i[j] == cosim[angka]:
+                panjangkitab= 0
+                for iterkitab in range(len(kitab)):
+                    panjangkitab = panjangkitab + len(kitab[iterkitab])
+                    if j <= panjangkitab:
+                        tessplit = kitab[iterkitab][0].split(',')
+                        print('Nama Kitab {} halaman ke {}'.format(namakitab[iterkitab],tessplit[4]))
+                        print('isi kitab', tessplit[5])
+                        finaloutput.append({'namakitab': namakitab[iterkitab], 'halaman': tessplit[4], 'isikitab': tessplit[5]})
+                        break
+        angka += 1
+    return finaloutput
+
+if __name__ == "__main__":
+    # text = "الدليل"
+    text = "القياس"
+    res = getResult(text)
+    print("===========================================")
+    print(res)
